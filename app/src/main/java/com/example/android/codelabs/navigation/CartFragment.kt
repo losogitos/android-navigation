acf36c6a46178bfbe1787f2c4c32d9ea45fb30e3
Łukasz Ciupa <losogitos@gmail.com>
2018-10-04 16:24:19 +0200

    Cart destination.

---
+++ app/src/main/java/com/example/android/codelabs/navigation/CartFragment.kt
@@ -0,0 +1,17 @@
+package com.example.android.codelabs.navigation
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CartFragment : Fragment() {
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+							  savedInstanceState: Bundle?): View? {
+		return inflater.inflate(R.layout.fragment_cart, container, false)
+	}
+}
